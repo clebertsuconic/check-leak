b43c8fcf9a8448e07bb046d36e1226f1aa8838b7
clebert <clebert.suconic@gmail.com>
2023-02-26 11:27:40 -0500

    Removing inline as that is giving problems in some platforms

--- check-leak/src/main/c/compile-linux.sh
+++
@@ -1,2 +0,0 @@
-gcc -Dcheck_leak_native_EXPORTS -I. -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -g -std=gnu11 -Wall  -fPIC -o agent.c.o -c agent.c
-gcc -fPIC -g -std=gnu11 -Wall  -shared -Wl,-soname,libcheckleak.so -o ../resources/platforms-lib/linux-amd64/libcheckleak.so agent.c.o 
